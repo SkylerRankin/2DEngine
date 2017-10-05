d2bcfd08195209b184f76bbf34e5849ab2b63ebd
Skyler <skylerrankin@gmail.com>
2017-10-05 17:27:39 -0400

    removed closure to fix reference errors

--- animator.js
+++ animator.js
@@ -1,62 +1,61 @@
-var animator = (function() {
+function animator(path, width, height) {
 
-  var delay;
-  var elapsed_frames = 0;
-  var set = {};
-  var running = false;
-  var sprites = [];
-  var current_frame = 0;
-  var currentAnimation = '';
+  this.delay;
+  this.elapsed_frames = 0;
+  this.set = {};
+  this.running = false;
+  this.sprites = [];
+  this.current_frame = 0;
+  this.currentAnimation = '';
 
-  return function(path, width, height) {
+  spritesheet.getSprites(path, width, height, function(_sprites, self) { self.sprites = _sprites; console.log('sprites loaded');}, this);
 
-    spritesheet.getSprites(path, width, height, function(_sprites) { sprites = _sprites; console.log('sprites loaded')});
+  this.drawFrame = function(ctx, x, y) {
 
-    this.drawFrame = function(ctx, x, y) {
+    if (!this.running) return;
 
-      if (!running) return;
-
-      if (elapsed_frames >= delay) {
-        elapsed_frames = 0;
-        ctx.drawImage(sprites[set[currentAnimation][current_frame++]], x, y);
-        if (current_frame >= set[currentAnimation].length)
-          current_frame = 0;
-      } else {
-        ctx.drawImage(sprites[set[currentAnimation][current_frame]], x, y); //allow for the current frame to be repeated if there is a frame delay
-        elapsed_frames++;
-      }
-
-    };
-
-    this.setAnimation = function(name) {
-      if (set.hasOwnProperty(name)) {
-        currentAnimation = name;
-        current_frame = 0;
-      }
-
-      return this;
-    } //set the current animation to use
-
-    this.getAnimation = function() {
-      return currentAnimation;
+    if (this.elapsed_frames >= this.delay) {
+      this.elapsed_frames = 0;
+      ctx.drawImage(this.sprites[this.set[this.currentAnimation][this.current_frame++]], x, y);
+      if (this.current_frame >= this.set[this.currentAnimation].length)
+        this.current_frame = 0;
+    } else {
+      if (this.sprites[this.set[this.currentAnimation][this.current_frame]])
+      ctx.drawImage(this.sprites[this.set[this.currentAnimation][this.current_frame]], x, y); //allow for the current frame to be repeated if there is a frame delay
+      this.elapsed_frames++;
     }
 
-    this.setAnimationSet = function(group) {
-      set = group;
-      return this;
-    } //set the group of animations to choose from
-    this.setFrameDelay = function(_delay) {
-      delay = _delay;
-      return this;
-    } //set frame skips for slower animations
-    this.start = function() {
-      running = true;
-    } //sets the animation to run so its used by draw()
-    this.stop = function() {
-      running = false;
-    }
-    this.running = function() {
-      return running;
-    };
   };
-})();
+
+  this.setAnimation = function(name) {
+    if (this.set.hasOwnProperty(name)) {
+      this.currentAnimation = name;
+      this.current_frame = 0;
+    }
+
+    return this;
+  } //set the current animation to use
+
+  this.getAnimation = function() {
+    return this.currentAnimation;
+  }
+
+  this.setAnimationSet = function(group) {
+    this.set = group;
+    return this;
+  } //set the group of animations to choose from
+  this.setFrameDelay = function(_delay) {
+    this.delay = _delay;
+    return this;
+  } //set frame skips for slower animations
+  this.start = function() {
+    this.running = true;
+  } //sets the animation to run so its used by draw()
+  this.stop = function() {
+    this.running = false;
+  }
+  this.running = function() {
+    return this.running;
+  };
+
+}
